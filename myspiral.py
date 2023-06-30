03453f406b33204721c73c3458f4b1f24ea5a2c3
Nixon-Diranshini1104 <135490935+Nixon-Diranshini1104@users.noreply.github.com>
2023-06-30 22:16:34 +0530

    Add files via upload

---
+++ myspiral.py
@@ -0,0 +1,18 @@
+import turtle
+
+from itertools import cycle
+color=cycle(['red', 'orange', 'yellow', 'green', 'blue','purple'])
+
+def draw_circle (size, angle, shift):
+    turtle.bgcolor (next (color))
+    turtle.pencolor (next (color))
+    turtle.circle(size)
+    turtle.right (angle)
+    turtle.forward(shift)
+    draw_circle(size+5, angle+1,shift+1)
+
+turtle.bgcolor('black')
+turtle.speed('fastest')
+turtle.pensize(10)
+draw_circle(15,0,1)
+
